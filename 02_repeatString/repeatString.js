a787b9f8080280b712f79bc06ed36c70d90b5f6a
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-02-22 00:40:30 +0700

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return 'ERROR';
+    } else {
+        let returnString = "";
 
+        for (let i = 0; i < times; i++) {
+            returnString += string;
+        }
+        
+        return returnString;
+}
 };
 
 // Do not edit below this line
