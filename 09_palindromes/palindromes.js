f5a3e59b070645c2d5e176a549af455ddfc89b02
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-02-23 00:25:06 +0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Remove whitespace inside string, convert to lowercase, then
+    // filter for valid characters
+    filteredArray = string.replace(/\s+/g, '').toLowerCase().split("").filter(char => {
+        return /[a-z0-9 ]/.test(char);
+    })
 
+    filteredString = filteredArray.join("");
+    reversedFilteredString = filteredArray.reverse().join("");
+
+    return filteredString === reversedFilteredString;
 };
 
 // Do not edit below this line
