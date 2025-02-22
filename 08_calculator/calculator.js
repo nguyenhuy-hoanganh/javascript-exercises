a1217f6cdce8401a352c38b367327a9bcdef1a02
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-02-22 23:53:01 +0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let finalSum = 0;
+  for (let num of array) {
+    finalSum += num;
+  }
+  return finalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let finalProduct = 1;
+  for (let num of array) {
+    finalProduct *= num;
+  }
+  return finalProduct;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+	return num * factorial(num - 1);
 };
 
 // Do not edit below this line
