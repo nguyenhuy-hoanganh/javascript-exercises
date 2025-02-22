82c94738292277074757755046c57b76d4bc9652
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-02-23 00:50:43 +0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    position = parseInt(position);
 
+    if (position < 0) {
+        return "OOPS";
+    } 
+    if (position === 0) {
+        return 0;
+    }
+
+    fibArray = [1, 1];
+
+    // Add to the array, up to the given position
+    for (let i = 0; i < position - 2; i++) {
+        newNum = fibArray.at(fibArray.length - 1) + fibArray.at(fibArray.length - 2)
+        fibArray.push(newNum);
+    }
+
+    return fibArray[position - 1];
 };
 
 // Do not edit below this line
