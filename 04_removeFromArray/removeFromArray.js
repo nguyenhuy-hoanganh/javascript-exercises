db941c04fa95df3c2c94b833d21f5cd2c626c8c0
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-02-22 22:16:36 +0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedElements) {
+    for (let element of removedElements) {
+        while (array.indexOf(element) != -1) {
+            array.splice(array.indexOf(element), 1)
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
