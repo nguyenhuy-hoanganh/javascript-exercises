a6921ddc69e81158561ecb3611aa15d2fd66059f
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-02-23 14:00:12 +0700

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let titlesArray = [];
+    for (let book of books) {
+      titlesArray.push(book.title)
+    }
+  
+  return titlesArray;
 };
+
+// More elegant solution below, using map()
+// const getTheTitles = function (array) {
+//   return array.map((book) => book.title);
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
