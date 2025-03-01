40b68415f5d09f829a435bb520aaad0355bd4592
Nguyen Huy Hoang Anh <hoanganh.nguyenhuy@protonmail.com>
2025-03-01 17:31:48 +0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const determineAge = function (person) {
+  if (person.hasOwnProperty("yearOfDeath")) {
+    age = person.yearOfDeath - person.yearOfBirth;
+  } else {
+    age = new Date().getFullYear() - person.yearOfBirth;
+  }
+  return age;
+};
 
+const findTheOldest = function (people) {
+  peopleDescendingAge = people
+  .map(person => {
+    return { name: person.name, age: determineAge(person) };
+  })
+  .toSorted((p1, p2) => p2.age - p1.age);
+  
+  return peopleDescendingAge[0];
 };
 
 // Do not edit below this line
